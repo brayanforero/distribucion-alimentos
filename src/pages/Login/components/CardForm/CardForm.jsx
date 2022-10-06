885ddf480b8875154e88c8c2b84d3163464eb88b
unknown <bforero.engineer@gmail.com>
2022-10-06 09:22:43 -0400

    :bug: [UPDATE] fix the card login size in small scree

--- src/pages/Login/components/CardForm/CardForm.jsx
+++ src/pages/Login/components/CardForm/CardForm.jsx
@@ -1,15 +1,15 @@
 import Logo from '@/assets/clap-logo.png'
 function CardForm({ children }) {
   return (
-    <div className=" bg-white opacity-90 w-full max-w-sm  rounded-lg shadow-lg py-5 px-8">
-      <div className="block mb-5 text-center">
-        <h1 className="text-3xl font-semibold text-gray-800 opacity-90">
+    <div className="bg-white opacity-90 w-full max-w-sm  rounded-lg shadow-lg py-5 px-8">
+      <div className="block mb-2 xl:mb-4 text-center">
+        <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 opacity-90">
           Inicio de Sesión
         </h1>
       </div>
       <div>
         <img
-          className="block mx-auto w-[220px] max-w-[320px]"
+          className="block mx-auto w-[140px] xl:w-[220px]"
           src={Logo}
           alt="clap logo"
         />
